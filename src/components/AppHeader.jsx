50a5d85ef6855ba2f72761309a1373440240fc94
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-06-12 21:16:04 +0200

    feat: nav-bar updated

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -17,17 +17,34 @@ const AppHeader = () => {
   ];
 
   return (
-  <header>
-    <nav>
-      <ul className="d-flex justify-content-center">
-        {navLinks.map((curLink, index) => (
-          <li key={index} className="mx-3">
-            <NavLink to={curLink.url}>{curLink.title}</NavLink>
-          </li>
-        ))}
-      </ul>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">
+          Navbar
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            {navLinks.map((curLink, index) => (
+              <NavLink key={index} className="nav-link" to={curLink.url}>
+                {curLink.title}
+              </NavLink>
+            ))}
+          </div>
+        </div>
+      </div>
     </nav>
-  </header>
   );
 };
 
